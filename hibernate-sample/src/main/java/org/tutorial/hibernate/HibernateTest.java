7276727ece3584babe3b54a1f050182d91086624
abhinash.kumar <abhinash.kumar@192.168.1.80>
2021-05-18 17:31:06 -0500

    setup before HQL tutorial

--- hibernate-sample/src/main/java/org/tutorial/hibernate/HibernateTest.java
+++ hibernate-sample/src/main/java/org/tutorial/hibernate/HibernateTest.java
@@ -12,37 +12,6 @@ import org.tutorial.hibernate.model.Vehicle;
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		UserDetails user = new UserDetails();
-		user.setUserName("Abhinash");
-		Address address = new Address();
-		address.setCity("Bokaro");
-		address.setPinCode("827013");
-		Address address2 = new Address();
-		address2.setCity("Kolkata");
-		address2.setPinCode("03410");
-		user.getUserAddresses().add(address);
-		user.getUserAddresses().add(address2);
-		Vehicle myVehicle = new Vehicle();
-		myVehicle.setVehicleName("Motorcycle");
-		Vehicle myVehicle2 = new Vehicle();
-		myVehicle2.setVehicleName("Car");
-		user.getVehicles().add(myVehicle);
-		user.getVehicles().add(myVehicle2);
-		
-		//inheritance T 17
-		TwoWheeler bike = new TwoWheeler();
-		bike.setVehicleName("Pulsar");
-		bike.setSteeringhandle("Bike steering handle");
-		
-		FourWheeler car = new FourWheeler();
-		car.setVehicleName("Accord");
-		car.setSteeringWheel("Car steering wheel");
-		
-		
-		
-//		myVehicle.getUserList().add(user);
-//		myVehicle2.getUserList().add(user);
-		//user.setVehicle(myVehicle);
 		//creating hibernate session factory
 		Configuration config = new Configuration();
 		config.configure();
@@ -51,23 +20,10 @@ public class HibernateTest {
 		Session session = sessionFactory.openSession();
 		//before saving data in db, we need to open a transaction
 		session.beginTransaction();
-		session.save(user);
-		session.save(bike);
-		session.save(car);
+		
 		//once save is done, we need to end the transaction.
 		session.getTransaction().commit();
 		//ideally below should in finally block.
-		session.close();
-		
-		//user = null;
-		
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-		
-		
-		Vehicle vehicle = session.get(Vehicle.class, 2);
-		vehicle.getUserList();
-		session.getTransaction().commit();
 		session.close();
 		/**
 		//to get the data from database. we passed 1 which is the primary key.
