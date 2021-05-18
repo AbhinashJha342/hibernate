b972e8f68aebdaa7161241b4515470ad3718a927
abhinash.kumar <abhinash.kumar@192.168.1.80>
2021-05-18 13:00:20 -0500

    updating for T20

--- hibernate-sample/src/main/java/org/tutorial/hibernate/model/Vehicle.java
+++ hibernate-sample/src/main/java/org/tutorial/hibernate/model/Vehicle.java
@@ -3,14 +3,13 @@ package org.tutorial.hibernate.model;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.DiscriminatorType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -20,8 +19,9 @@ import org.hibernate.annotations.NotFoundAction;
 
 @Entity
 @Table(name = "VEHICLE")
-@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)	// added for T17 and T18 for inheritance
+//@Inheritance(strategy=InheritanceType.TABLE_PER_CLASS)	// added for T17 and T18 for inheritance
 //@DiscriminatorColumn(name="VEHICLE_TYPE", discriminatorType=DiscriminatorType.STRING) // to change the name for auto-generated dType column.
+@Inheritance(strategy=InheritanceType.JOINED) // we use this strategy to create a new table and remove any extra null values that we get by simple TABLE_PER_CLASS strategy
 public class Vehicle {
 
 	@Id@GeneratedValue(strategy=GenerationType.AUTO)
@@ -32,6 +32,7 @@ public class Vehicle {
 	//private UserDetails userDetails; //user is a reserved keyword, and should not used. error: Error executing DDL "alter table VEHICLE drop constraint FKq9acqfnl2hkuhrm1la9gamh8f" via JDBC Statement
 	//@ManyToMany(cascade=CascadeType.ALL)
 	@ManyToOne
+	@JoinColumn(name="USER_ID")
 	@NotFound(action=NotFoundAction.IGNORE)
 	private UserDetails user;
 	
