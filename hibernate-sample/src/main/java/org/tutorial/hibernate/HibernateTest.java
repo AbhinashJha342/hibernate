533b131a9353793b6fd90c0870dd8990388a2889
abhinash.kumar <abhinash.kumar@192.168.1.80>
2021-06-01 13:23:47 -0500

    Criteria API- V31

--- hibernate-sample/src/main/java/org/tutorial/hibernate/HibernateTest.java
+++ hibernate-sample/src/main/java/org/tutorial/hibernate/HibernateTest.java
@@ -2,9 +2,11 @@ package org.tutorial.hibernate;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.query.Query;
 import org.tutorial.hibernate.model.Address;
 import org.tutorial.hibernate.model.UserDetails;
@@ -26,11 +28,29 @@ public class HibernateTest {
 		Vehicle myVehicle = new Vehicle();
 		myVehicle.setVehicleName("Motorcycle");
 		
+		UserDetails newUser1 = new UserDetails();
+		newUser1.setUserName("Alicia");
+		
+		UserDetails newUser2 = new UserDetails();
+		newUser2.setUserName("Raman");
+		
 		//creating hibernate session factory
 		Configuration config = new Configuration();
 		config.configure();
 		// local SessionFactory bean created
 		SessionFactory sessionFactory = config.buildSessionFactory();
+		
+//		Session session1 = sessionFactory.openSession();
+//		session1.beginTransaction();
+//		session1.save(newUser1);
+//		session1.getTransaction().commit();
+//		
+//		Session session2 = sessionFactory.openSession();
+//		session2.beginTransaction();
+//		session2.save(newUser2);
+//		session2.getTransaction().commit();
+		
+		
 		Session session = sessionFactory.openSession();
 		//before saving data in db, we need to open a transaction
 		session.beginTransaction();
@@ -38,26 +58,32 @@ public class HibernateTest {
 		//query.setInteger("minVehicleId", Integer.parseInt(minVehicleId));
 		//query.setString("vehicleName", vehicleName);
 		session.save(newUser);
-		Query query = session.getNamedQuery("UserDetails.byID");
-		query.setInteger(0,1);
+		session.save(newUser1);
+		session.save(newUser2);
+		//using Criteria API
+		Criteria criteria = session.createCriteria(UserDetails.class);
+		criteria.add(Restrictions.eq("userName", "Abhinash"));
 		
-		List<UserDetails> userDeatils = (List<UserDetails>)query.list();
+		//Query query = session.getNamedQuery("UserDetails.byID");
+		//query.setInteger(0,1);
+		
+		List<UserDetails> userDeatils = (List<UserDetails>)criteria.list();
 		for(UserDetails user : userDeatils) {
-			System.out.println(user.getUserId());
+			System.out.println("########"+user.getUserId());
 		}
 		
 		Query query1 = session.getNamedQuery("UserDetails.byName");
-		query1.setString(0, "Abhinash");
+		query1.setString(0, "Alicia");
 		
 		List<UserDetails> userDeatils1 = (List<UserDetails>)query1.list();
 		for(UserDetails user : userDeatils1) {
-			System.out.println(user.getUserName());
+			System.out.println("******"+user.getUserName());
 		}
 		
 		//once save is done, we need to end the transaction.
 		session.getTransaction().commit();
-		//ideally below should in finally block.
-		session.close();
+		//ideally below should in finally block.		
+		
 		/**
 		//to get the data from database. we passed 1 which is the primary key.
 		
